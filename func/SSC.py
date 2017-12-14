f07b4752345d828516fb9825b902ef01749feadf
gitmetocean <remy@remy-pc.np.metocean.co.nz>
2017-12-15 11:38:08 +1300

    get all area from one element

--- func/SSC.py
+++ func/SSC.py
@@ -43,7 +43,7 @@ def get_nodes(mesh,vertices):
 	ref=np.zeros((mesh.nodes.shape[0],1))
 	nodes_coor = np.hstack((mesh.nodes[:,0:2],ref))
 #populate (x,y) and elevation information for each triangular element
-	tri = np.zeros((len(Elems),3,4))
+	tri = np.zeros((len(Elems),3,3))
 	for itri in range(len(Elems)):
 		for ivert in range(3):
 			tri[itri,ivert,:] = nodes_coor[mesh.elems[Elems[itri]][ivert]] 
